019a826abba36a6248c75ec69d80bee397921ffe
cgpu <chatzipantsiou@gmail.com>
2019-04-23 19:12:09 +0300

    updated-script-to-download-on-the-fly-the-csv

--- 0_mkdir_dwnload.sh
+++ 0_mkdir_dwnload.sh
@@ -1,3 +1,6 @@
+# Download .csv file with 30 selected openSNP 23andme SNPset files
+wget https://s3-eu-west-1.amazonaws.com/deploit-tutorial/openSNP_23andme_txt_n30.csv 
+
 # Create directory only if the first command succeeds
 grep -v "^#" openSNP_23andme_txt_n30.csv | cut -d',' -f2 > urlfile.txt  && mkdir 23andme_downloads_dir/
 
