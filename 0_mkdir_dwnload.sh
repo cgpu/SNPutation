80719b0ea4f71e106510a3434912cf089cd80682
cgpu <chatzipantsiou@gmail.com>
2019-04-23 19:09:24 +0300

    updated-mkdir-command-to-execute-only-if-csv-exists

--- 0_mkdir_dwnload.sh
+++ 0_mkdir_dwnload.sh
@@ -1,6 +1,6 @@
-grep -v "^#" openSNP_23andme_txt_n30.csv | cut -d',' -f2 > urlfile.txt
+# Create directory only if the first command succeeds
+grep -v "^#" openSNP_23andme_txt_n30.csv | cut -d',' -f2 > urlfile.txt  && mkdir 23andme_downloads_dir/
 
-mkdir 23andme_downloads_dir/
-
+# Sequentially download (wget) files from URL list
 wget -i urlfile.txt -P 23andme_downloads_dir/
 
