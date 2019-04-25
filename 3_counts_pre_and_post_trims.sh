fc94d5ee22dd90f14ec175834ba2be94fff5d991
cgpu <38183826+cgpu@users.noreply.github.com>
2019-04-25 15:38:20 +0100

    Delete 3_counts_pre_and_post_trims.sh

--- 3_counts_pre_and_post_trims.sh
+++
@@ -1,3 +0,0 @@
-for file in YXMTless_23andme_files_dir/*.23andme.* ; do cat $file | wc -l | tr "\n" "\t" && echo `basename $file` > counts_post_YXMT_trim.txt; done
-
-for file in dashless_YXMTless_23andme_files/*.23andme.* ; do cat $file | wc -l | tr "\n" "\t" && echo `basename $file` > counts_post_YXMT_dash_trims.txt; done
